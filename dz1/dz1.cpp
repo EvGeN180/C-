042624e47784fe3ff9ce468a6930c3f4aa78204c
EvGeN180 <malyna.evgen2010@gmail.com>
2024-11-04 12:24:43 +0200

    Add project files.

---
+++ dz1/dz1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+//int main()
+//{
+//    double R1, R2, R3, RO;
+//    cout << "Enter R1.R2,R3 ";
+//    cin >> R1 >> R2 >> R3;
+//
+//    RO = 1 /(( 1/R1 )+ (1/R2) + (1/R3));
+//    cout << "RO = " << RO << endl;
+//}
+
+//int main1() {
+//	const float p = 3.14f;
+//	float L;
+//	cout << "Enter L" << endl;
+//	cin >> L;
+//	float R = L / (2 * p);
+//	float S = p * R * R;
+//
+//	cout << "R= " << R << endl;
+//	cout << "S= " << S << endl;
+//
+//}
+
+
+
+
+
+//int main2()
+//{
+//	const float p = 3.14f;
+//	int l;
+//
+//	cout << "Enter l: ";
+//	cin >> l;
+//
+//	cout << "R=" << l/ 2 * p << endl<<"S="<<p* ((l / 2 * p)* (l / 2 * p));
+//
+//}
+
+
+
+//int main2()
+//{
+//	int v, t, a;
+//
+//	cout << "Enter V,T,A: ";
+//	cin >> v;
+//	cin >> t;
+//	cin >> a;
+//
+//	cout << "S = " << (v * t + (a * (t * t) / 2)) << endl;
+//
+//}
+
