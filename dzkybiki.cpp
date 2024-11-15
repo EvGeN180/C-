06096e9d2287f62b01b969f8d08db3d20bf2c6c9
EvGeN180 <malyna.evgen2010@gmail.com>
2024-11-15 20:41:10 +0200

    Add files via upload

---
+++ dzkybiki.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+#include <random>;
+
+int main()
+{
+    srand(time(0));
+    int user_result = 0, computer_result = 0;
+    string username;
+    cout << "--------------CUBS--------------"<<endl;
+    cout << "Enter username" << endl;
+    cin >> username;
+    for (size_t i = 1; i < 4; i++) {
+        cout << "-----------------------" << i << "round-----------------------" << endl;
+        cout << username << " throw cubs" << endl;
+        int ucub1 = rand() % 6 + 1;
+        int ucub2 = rand() % 6 + 1;
+        cout << ucub1 << ","<<ucub2 << endl;
+        user_result += ucub1 + ucub2;
+        cout << "Computer" << " throw cubs" << endl;
+        int ccub1 = rand() % 6 + 1;
+        int ccub2 = rand() % 6 + 1;
+        cout << ccub1<<"," << ccub2 << endl;
+        computer_result += ccub1 + ccub2;
+    }
+    cout << "--------------------Final results----------------------" << endl;
+    cout << username << "sum=" << user_result << endl;
+    cout << "Computer" << "sum=" << computer_result << endl;
+    if (computer_result > user_result) {
+        cout << "You lose, computer won!" << endl;
+    }
+    else if (computer_result < user_result) {
+        cout << "Computer lose, You won!Congratulations!" << endl;
+    }
+    else {
+        cout << "Draw!" << endl;
+    }
+   
+   
+
+}
+
