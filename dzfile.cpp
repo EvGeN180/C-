aab3c5f5521a17bd412f5ebe60e8b86afb28ff87
EvGeN180 <malyna.evgen2010@gmail.com>
2025-01-14 15:40:32 +0200

    Add files via upload

---
+++ dzfile.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//using namespace std;
+//
+//
+//int main()
+//{
+//    string file = "test.txt";
+//    string newFile = "C:\\Users\\Malin\\OneDrive\\Робочий стіл\\newFile.txt";
+//    ifstream start_file(file, ios::app);
+//    ofstream end_file(newFile);
+//
+//    if (start_file.is_open()) {
+//
+//        cout << "FILE WAS WRITTEN" << endl;
+//
+//    }
+//    string text = " ";
+//    while (getline(start_file, text)) {
+//        end_file << text;
+//    }
+//    start_file.close();
+//    remove("test.txt");
+//}
+
+
+
