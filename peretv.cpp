3e0c5e41cc08f32f30d9dcbee81a8eff3b53e62f
EvGeN180 <malyna.evgen2010@gmail.com>
2025-07-14 13:48:08 +0300

    Add files via upload

---
+++ peretv.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cmath>      
+using namespace std;
+
+class Transport {
+public:
+    virtual void info() const { cout << "Transport "; }
+    virtual ~Transport() = default;
+};
+
+class Bicycle : public Transport {
+public:
+    void info() const override { cout << "Bicycle "; }
+};
+
+class Car : public Transport {
+public:
+    void info() const override { cout << "Car "; }
+};
+
+class Moto : public Transport {
+public:
+    void info() const override { cout << "Moto"; }
+};
+
+void addUserNumber(int* ptr)          
+{
+    int add{};
+    cout << "Enter num: ";
+    cin >> add;
+    *ptr += add;                 
+}
+
+int main()
+{
+    cout << "Enter float num: ";
+    double d{};
+    cin >> d;
+    int i = static_cast<int>(round(d));
+
+    cout << "float: " << d << endl;
+    cout << "int   : " << i << endl;
+
+    Transport* base = new Car;       
+    base->info();
+
+    if (auto car = dynamic_cast<Car*>(base)) {
+        car->info();
+    }
+    delete base;
+
+    const int kConst10 = 10;
+    int value = kConst10;
+    addUserNumber(&value);
+    cout << value << '\n';
+
+}
