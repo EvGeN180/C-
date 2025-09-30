66ac8d494b1864bfd06358ba5a7ebe097e877271
EvGeN180 <malyna.evgen2010@gmail.com>
2025-09-30 12:19:49 +0300

    Create form.cpp

---
+++ form.cpp
@@ -0,0 +1,13 @@
+#include "Form1.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew Form1());
+    return 0;
+}
